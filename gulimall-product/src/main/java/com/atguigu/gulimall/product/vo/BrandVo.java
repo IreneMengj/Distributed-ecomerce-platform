d7efb906b90605977288b78870f8d1282445d99c
irene <mengjiayu2021@gmail.com>
2023-01-03 10:53:08 +0000

    First version of Gulimall

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/BrandVo.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.product.vo;
+
+public class BrandVo {
+    private Long brandId;
+    private String brandName;
+
+    public BrandVo() {
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+}
